b4649272c4d3a8d15a13e781f43b5567de85a9b2
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-02 06:27:56 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2428,24 +2428,17 @@ async def advantage_spell_chok(message):
     query = query.strip() + " movie"
     try:
         movies = await get_poster(search, bulk=True)
-    except Exception as e:
-        logger.exception("get_poster failed for query=%s: %s", query, e)
-        try:
-            k = await message.reply(script.I_CUDNT.format(message.from_user.mention))
-            await asyncio.sleep(60)
-            try:
-                await k.delete()
-            except Exception:
-                pass
-        except Exception:
-            pass
+    except:
+        k = await message.reply(script.I_CUDNT.format(message.from_user.mention))
+        await asyncio.sleep(60)
+        await k.delete()
         try:
             await message.delete()
-        except Exception:
+        except:
             pass
         return
     if not movies:
-        google = quote_plus(search)
+        google = search.replace(" ", "+")
         button = [
             [
                 InlineKeyboardButton(
