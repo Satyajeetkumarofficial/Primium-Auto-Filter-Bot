96158d6d2bacc09af6abe062c09d1cc075f8623f
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-03 14:15:50 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -17,7 +17,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("InfinityBotzz ~ EDITH")
+    return web.json_response("Telegram ~ ProBotXUpdate")
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
